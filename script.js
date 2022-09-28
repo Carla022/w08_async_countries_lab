0fbae06d491c82356f191c37081da92a6691e4e3
Carla022 <cs.morenol@outlook.com>
2022-09-28 23:01:48 +0100

    commit 3

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+//place button 
     const fetchCountries = () => {
 
-        request =  fetch ("https://restcountries.com/v3.1/all")
+       const requestPromise =  fetch ("https://restcountries.com/v3.1/all")
+       
+       const jsonDataPromise = requestPromise 
         .then((response) => {
            return response.json();
 
         })
-        .then((jsonData) => {
-            console.log(jsonData);
-
+        jsonDataPromise.then((jsonData) => {
+            console.log(jsonData.url);
         })
+        //     
+
+        // })
 
     }
     fetchCountries();
+
+    //fetching a response
+    const getCountries = async () => {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const jsonData = await response.json();
+
+        const countriesData = jsonData.name;
+
+    }
+
+    const countryBtn = document.querySelector("#countries-button");
+    countryBtn.addEventListener("click", getCountries)
 
 })
