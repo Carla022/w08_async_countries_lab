8c0f29b3935848fddac1dc4aa52f02e0f5b165a0
Carla022 <cs.morenol@outlook.com>
2022-09-29 10:33:25 +0100

    commit 4

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 window.addEventListener('DOMContentLoaded', () => {
-//place button 
-    const fetchCountries = () => {
-
-       const requestPromise =  fetch ("https://restcountries.com/v3.1/all")
-       
-       const jsonDataPromise = requestPromise 
-        .then((response) => {
-           return response.json();
-
-        })
-        jsonDataPromise.then((jsonData) => {
-            console.log(jsonData.url);
-        })
-        //     
-
-        // })
-
-    }
-    fetchCountries();
+    //place button 
 
     //fetching a response
-    const getCountries = async () => {
-        const response = await fetch("https://restcountries.com/v3.1/all");
-        const jsonData = await response.json();
+    const fetchCountries = async () => {
+ //fetch is an asynchronous funciton 
+        try {
 
-        const countriesData = jsonData.name;
+            const response = await fetch("https://restcountries.com/v3.1/all"); // returns a promise
+            //naming things in a pluralised way, means its a collection of things e.g. arrays
+            const jsonData = await response.json();
+    
+            createCountryList(jsonData);
+
+        } catch {
+            const h2 = document.querySelector("h2")
+            
+        }
+
+        
 
     }
+
+
+    //this allows the countries to be loaded without the need for a button:
+    // fetchCountries();
 
     const countryBtn = document.querySelector("#countries-button");
     countryBtn.addEventListener("click", getCountries)
